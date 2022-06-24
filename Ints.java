41e479a5f0e7c6d283e9ef244d75d8c40465bfa5
elis37 <104443314+elis37@users.noreply.github.com>
2022-06-24 19:56:41 +0500

    Create Ints.java

---
+++ Ints.java
@@ -0,0 +1,9 @@
+es (6 sloc)  158 Bytes
+
+package JavaPatterns_2_1_Structural;
+
+public interface Ints {
+    int sum(int arg0, int arg1);
+    int mult(int arg0, int arg1);
+    int pow(int a, int b);
+}
